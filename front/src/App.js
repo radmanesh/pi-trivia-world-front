a0d654cbcd41e10f00a55e65f2071de6d304c08e
Arman Radmanesh <Radmanesh@gmail.com>
2024-05-11 12:14:08 -0500

    Improvments

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Game from "./components/Game";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true }}>
       <div style={{ textAlign: "center" }}>
-        <Route exact path="/" component={Game} />
+        <Routes>
+          <Route exact path="/" element={<Game />} />
+        </Routes>
       </div>
     </Router>
   );
